3260da68bda4ec5f479ceb4bf46bf4ea77017822
Ayush Gupta <ag6320226@gmail.com>
2026-01-23 23:07:41 +0530

    js and html combine

---
+++ indexx.js
@@ -0,0 +1,17 @@
+
+console.log("hellow ha bhai");
+
+let btn = document.querySelector("buton")
+let body = document.querySelector("body")
+
+    btn.addEventListener("click",function(){
+        body.style.backgroundColor="blue"
+        
+    
+    })
+ 
+
+
+
+
+ 
